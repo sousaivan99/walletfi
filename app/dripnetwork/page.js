c7f5bab883b0e41e17b91bbc9c9dc4fb831f842a
Sousa Ivan <sousa.ivan99@gmail.com>
2023-06-06 13:49:04 +0200

    dripnetwork page status changed to in Development

---
+++ app/dripnetwork/page.js
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from "react";
+import tokenList from "@crypto/tokenContracts";
+import "@styles/swap.css";
+
+const swap = () => {
+  const [modalOpen, setModelOpen] = useState(false);
+  return (
+    <>
+      <div>In development!</div>
+    </>
+  );
+};
+
+export default swap;
